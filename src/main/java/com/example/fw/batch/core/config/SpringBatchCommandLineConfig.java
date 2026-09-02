dd1773997cc8c8ca9fbcd9483e5bae2f4b2e405b
mysd33 <mysd.33@gmail.com>
2026-09-02 22:02:38 +0900

    ジョブフロー実行支援に関する機能のプロパティの修正

--- src/main/java/com/example/fw/batch/core/config/SpringBatchCommandLineConfig.java
+++ src/main/java/com/example/fw/batch/core/config/SpringBatchCommandLineConfig.java
@@ -1,5 +1,11 @@
 package com.example.fw.batch.core.config;
 
+import com.example.fw.batch.core.exception.ExceptionHandler;
+import com.example.fw.batch.core.launch.DefaultJobLauncherApplicationRunner;
+import com.example.fw.batch.core.listener.CommandLineJobExecutionListener;
+import com.example.fw.batch.jobflow.config.JobflowConfigurationProperties;
+import com.example.fw.batch.jobflow.converter.SfnJobParametersConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.converter.DefaultJobParametersConverter;
 import org.springframework.batch.core.converter.JobParametersConverter;
 import org.springframework.batch.core.launch.JobOperator;
@@ -14,42 +20,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
 
-import com.example.fw.batch.core.exception.ExceptionHandler;
-import com.example.fw.batch.core.launch.DefaultJobLauncherApplicationRunner;
-import com.example.fw.batch.core.listener.CommandLineJobExecutionListener;
-import com.example.fw.batch.jobflow.config.JobflowConfigurationProperties;
-import com.example.fw.batch.jobflow.converter.SfnJobParametersConverter;
-
-import lombok.RequiredArgsConstructor;
-
 /// SpringBatchのコマンドライン実行用の設定クラス
 @Configuration
 @RequiredArgsConstructor
-@EnableConfigurationProperties({ SpringBatchConfigurationProperties.class, JobflowConfigurationProperties.class })
+@EnableConfigurationProperties({SpringBatchConfigurationProperties.class,
+    JobflowConfigurationProperties.class})
 @ConditionalOnProperty(prefix = SpringBatchConfigurationProperties.PROPERTY_PREFIX, name = "type", havingValue = "commandline")
 public class SpringBatchCommandLineConfig {
 
     /// ジョブパラメータのコンバータの定義（ジョブフロー未使用）
     @Bean
-    @ConditionalOnProperty(prefix = JobflowConfigurationProperties.PROPERTY_PREFIX, name = "enable", havingValue = "false", matchIfMissing = false)
-    JobParametersConverter jobParametersConverterNotUsingJobflow() {
+    @ConditionalOnProperty(prefix = JobflowConfigurationProperties.PROPERTY_PREFIX, name = "enable", havingValue = "false", matchIfMissing = true)
+    JobParametersConverter jobParametersConverterNotUsingJobFlow() {
         return new DefaultJobParametersConverter();
     }
 
     /// ジョブフローによるコマンド起動用のJobParametersConverterの定義
     @Bean
-    @ConditionalOnProperty(prefix = JobflowConfigurationProperties.PROPERTY_PREFIX, name = "enable", havingValue = "true", matchIfMissing = true)
+    @ConditionalOnProperty(prefix = JobflowConfigurationProperties.PROPERTY_PREFIX, name = "enable", havingValue = "true", matchIfMissing = false)
     JobParametersConverter jobParametersConverterForJobFlow() {
         return new SfnJobParametersConverter();
     }
 
     /// コマンドライン実行用のJobLauncherApplicationRunnerのBean定義<br>
     @Bean
-    JobLauncherApplicationRunner jobLauncherApplicationRunner(JobOperator jobOperator, BatchProperties properties,
-            JobRepository jobRepository, JobflowConfigurationProperties jobflowConfigurationProperties,
-            Environment env) {
+    JobLauncherApplicationRunner jobLauncherApplicationRunner(JobOperator jobOperator,
+        BatchProperties properties,
+        JobRepository jobRepository, JobflowConfigurationProperties jobflowConfigurationProperties,
+        Environment env) {
         var runner = new DefaultJobLauncherApplicationRunner(jobOperator, jobRepository,
-                jobflowConfigurationProperties, env);
+            jobflowConfigurationProperties, env);
         String jobName = properties.getJob().getName();
         if (StringUtils.hasText(jobName)) {
             runner.setJobName(jobName);
@@ -59,7 +59,8 @@ public class SpringBatchCommandLineConfig {
 
     /// ジョブの実行に関わる例外ハンドリング、ログ出力機能の設定
     @Bean
-    JobExecutionListener defaultJobExecutionListenerForCommandLine(ExceptionHandler defaultExceptionHandler) {
+    JobExecutionListener defaultJobExecutionListenerForCommandLine(
+        ExceptionHandler defaultExceptionHandler) {
         return new CommandLineJobExecutionListener(defaultExceptionHandler);
     }
 }
--- src/main/java/com/example/fw/batch/jobflow/config/JobflowCommonConfig.java
+++ src/main/java/com/example/fw/batch/jobflow/config/JobflowCommonConfig.java
@@ -1,32 +1,33 @@
 package com.example.fw.batch.jobflow.config;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
 import com.example.fw.batch.jobflow.sfn.repository.SfnRepositoryPackage;
 import com.example.fw.batch.jobflow.sfn.repository.SfnTaskResultRepository;
 import com.example.fw.batch.jobflow.sfn.service.DefaultSfnTaskResultPersistService;
 import com.example.fw.batch.jobflow.sfn.service.SfnTaskResultPersistService;
 import com.example.fw.common.systemdate.SystemDate;
 import com.example.fw.common.systemdate.config.SystemDateConfig;
-
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /// ジョブフローによる起動用の設定クラス
+@ConditionalOnProperty(prefix = JobflowConfigurationProperties.PROPERTY_PREFIX, name = "enable", havingValue = "true", matchIfMissing = false)
 @Configuration
 @RequiredArgsConstructor
 @EnableConfigurationProperties(JobflowConfigurationProperties.class)
 @Import(SystemDateConfig.class)
-@MapperScan(basePackageClasses = { SfnRepositoryPackage.class }, annotationClass = Mapper.class)
+@MapperScan(basePackageClasses = {SfnRepositoryPackage.class}, annotationClass = Mapper.class)
 public class JobflowCommonConfig {
 
     /// ジョブフローのタスク結果を永続化するサービスのBean定義
     @Bean
-    SfnTaskResultPersistService sfnTaskResultPersistService(SfnTaskResultRepository repository, SystemDate systemDate) {
+    SfnTaskResultPersistService sfnTaskResultPersistService(SfnTaskResultRepository repository,
+        SystemDate systemDate) {
         return new DefaultSfnTaskResultPersistService(repository, systemDate);
     }
 
